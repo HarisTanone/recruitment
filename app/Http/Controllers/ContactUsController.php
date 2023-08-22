705ec9f428c46cb92fac3d08ad738036c30e8f82
haristanone <haris@krafthaus.co.id>
2023-08-22 21:30:47 +0700

    add api for contact and faq

---
+++ app/Http/Controllers/ContactUsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class ContactUsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $contactUs = DB::table('contact_us')->get();
+        return response()->json($contactUs);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $contactUs = DB::table('contact_us')->insertGetId([
+            'firstname' => $request->input('firstname'),
+            'lastname' => $request->input('lastname'),
+            'email' => $request->input('email'),
+            'message' => $request->input('message'),
+        ]);
+
+        return response()->json(['message' => 'Contact message created successfully', 'data' => $contactUs]);
+    }
+
+    public function show($id)
+    {
+        $contactUs = DB::table('contact_us')->find($id);
+        if (!$contactUs) {
+            return response()->json(['message' => 'Contact message not found'], 404);
+        }
+        return response()->json($contactUs);
+    }
+
+    public function destroy($id)
+    {
+        $contactUs = DB::table('contact_us')->find($id);
+        if (!$contactUs) {
+            return response()->json(['message' => 'Contact message not found'], 404);
+        }
+
+        DB::table('contact_us')->where('id', $id)->delete();
+
+        return response()->json(['message' => 'Contact message deleted']);
+    }
+}
---
+++ app/Http/Controllers/faqController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class faqController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $faqs = DB::table('faq')->get();
+        return response()->json($faqs);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'question' => 'required',
+            'answer' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $faqId = DB::table('faq')->insertGetId([
+            'question' => $request->input('question'),
+            'answer' => $request->input('answer'),
+            'status' => $request->input('status', 0),
+        ]);
+
+        $faq = DB::table('faq')->where('id', $faqId)->first();
+        return response()->json(['message' => 'FAQ created successfully', 'data' => $faq]);
+    }
+
+    public function show($id)
+    {
+        $faq = DB::table('faq')->find($id);
+        if (!$faq) {
+            return response()->json(['message' => 'FAQ not found'], 404);
+        }
+        return response()->json($faq);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $faq = DB::table('faq')->find($id);
+
+        if (!$faq) {
+            return response()->json(['message' => 'FAQ not found'], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'question' => 'required',
+            'answer' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        DB::table('faq')->where('id', $id)->update([
+            'question' => $request->input('question'),
+            'answer' => $request->input('answer'),
+            'status' => $request->input('status', 0),
+        ]);
+
+        $updatedFaq = DB::table('faq')->find($id);
+        return response()->json(['message' => 'FAQ updated successfully', 'data' => $updatedFaq]);
+    }
+
+    public function destroy($id)
+    {
+        $faq = DB::table('faq')->find($id);
+        if (!$faq) {
+            return response()->json(['message' => 'FAQ not found'], 404);
+        }
+
+        DB::table('faq')->where('id', $id)->delete();
+        return response()->json(['message' => 'FAQ deleted']);
+    }
+}
