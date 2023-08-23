dde4020902272df8edd1c5ed5c6ffdd417fdd362
weynand04 <wamaerkabor@gmail.com>
2023-08-23 23:57:08 +0700

    Redirect ke section FAQ

--- resources/views/user/pages/faq-pages.blade.php
+++ resources/views/user/pages/faq-pages.blade.php
@@ -42,17 +42,17 @@
 @endsection
 @section('script')
     <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        // Menangani klik pada tombol searchButton
-        document.getElementById("searchButton").addEventListener("click", function() {
-            // Temukan elemen tujuan dengan ID sectionDUA
-            var sectionDUA = document.getElementById("sectionDUA");
-            if (sectionDUA) {
-                // Perpindahan scroll ke bagian tujuan
-                sectionDUA.scrollIntoView({ behavior: "smooth" });
-            }
+        document.addEventListener("DOMContentLoaded", function() {
+            // Menangani klik pada tombol searchButton
+            document.getElementById("searchButton").addEventListener("click", function() {
+                // Temukan elemen tujuan dengan ID sectionDUA
+                var sectionDUA = document.getElementById("sectionDUA");
+                if (sectionDUA) {
+                    // Perpindahan scroll ke bagian tujuan
+                    sectionDUA.scrollIntoView({ behavior: "smooth" });
+                }
+            });
         });
-    });
     </script>
     <script>
         $(document).ready(function() {
